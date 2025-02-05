0ec266ef1a33c6450f5ea5c1437ed8b6c556a847
Gabriel Gatimu <geraldg652@gmail.com>
2025-02-05 22:41:17 +0300

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# HNG12 Stage 0 Backend Task
+
+A simple public API that returns basic information in JSON format.
+
+## Information Returned
+- Your registered email address.
+- The current datetime in ISO 8601 format (UTC).
+- The GitHub URL of the project's codebase.
+
+## Setup and Running
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/gatimugabriel/hng12-task1.git
---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from datetime import datetime
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def get_info():
+    response = {
+         "email": "geraldg652@gmail.com",
+        "current_datetime": datetime.utcnow().isoformat() + "Z",  
+        "github_url": "https://github.com/gatimugabriel/hng12-task1"
+    }
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
